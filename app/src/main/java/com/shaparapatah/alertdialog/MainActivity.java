8ce57fa134027c7ea514c84b26cd963c9cd0b637
Shaparapatah <mm.fedorchenko@gmail.com>
2021-08-29 18:02:03 +0300

    Сделал DialogAlert

--- app/src/main/java/com/shaparapatah/alertdialog/MainActivity.java
+++ app/src/main/java/com/shaparapatah/alertdialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.shaparapatah.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button alertDialog = findViewById(R.id.alertDialog);
+        alertDialog.setOnClickListener(clickListenerDialog1);
     }
+
+
+    private View.OnClickListener clickListenerDialog1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(R.string.exclamation)
+                    .setMessage(R.string.message)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    })
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    };
 }
